abc53cb03cd5c30f384b9f24762d63f0effdfd70
andriishchepaniak <48985381+andriishchepaniak@users.noreply.github.com>
2025-06-25 11:36:03 +0300

    Added routing and app skeleton (#1)
    Co-authored-by: andrewshchepaniak <andrii.shchepaniak@qualified-consult.com>

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <script setup>
+import Navbar from "./components/Navbar.vue";
+import Applications from "./components/Applications.vue";
+import Customers from "./components/Customers.vue";
+import NotFound from "./components/NotFound.vue";
+import Sidebar from "./components/Sidebar.vue";
+import { ref, computed } from "vue";
+
+const routes = {
+  '/': Applications,
+  '/customers': Customers
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
 
 <template>
+  <Navbar />
   <main>
-    <h1>Hello world from Vue</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-3 sidebar d-flex flex-row-reverse">
+          <Sidebar />
+        </div>
+        <div class="col-9">
+          <component :is="currentView" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
---
+++ src/components/Customers.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Customers</h1>
+</template>

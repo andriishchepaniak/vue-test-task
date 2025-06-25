e1b6c382ae182d9caf8a954afc1b61c935064b09
andriishchepaniak <48985381+andriishchepaniak@users.noreply.github.com>
2025-06-25 12:59:03 +0300

    Added creating and displaying customers logic (#2)
    Co-authored-by: andrewshchepaniak <andrii.shchepaniak@qualified-consult.com>

--- src/components/Customers.vue
+++ src/components/Customers.vue
@@ -1,6 +1,21 @@
 <script setup>
+import { ref } from 'vue';
+import { useAppStore } from '.././store/store.js'
+import AddCustomer from './AddCustomer.vue'
+import Customer from './Customer.vue'
+
+const appStore = useAppStore();
+
+const customers = ref(appStore.getCustomers());
+
 </script>
 
 <template>
-    <h1>Customers</h1>
+    <h2>Замовники</h2>
+
+    <AddCustomer />
+   
+    <br/>
+
+    <Customer v-for="customer in customers" :key="customer.id" :customer="customer"/>
 </template>

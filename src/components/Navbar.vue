f7efd781fd2ae1bdbf8a2e1767b44c42f9f97979
andrewshchepaniak <andrii.shchepaniak@qualified-consult.com>
2025-06-25 11:35:11 +0300

    Added routing and app skeleton

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+   <nav class="navbar bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+    </nav>
+</template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="#/">Заяви</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#/customers">Замовники</a>
+        </li>
+    </ul>
+</template>
